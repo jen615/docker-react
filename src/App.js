f168863eb4c461fa73c4567b3fb5fab35f450bda
jen615 <xjenghisx@gmail.com>
2020-08-19 18:37:15 -0700

    feature branch testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         ahhhh 😳
+         she succ me! 😳
         </p>
         <a
           className="App-link"
